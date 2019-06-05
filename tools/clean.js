b74ab846967e7c0c4a2572bc79bf632f4be46e6c
Michael Schwartz <mykesx@gmail.com>
2019-06-04 19:04:12 -0700

    ha -> robodomo

--- tools/clean.js
+++ tools/clean.js
@@ -1,16 +1,20 @@
-const mqtt   = require('mqtt'),
-      client = mqtt.connect('mqtt://ha')
+const mqtt = require("mqtt"),
+  client = mqtt.connect("mqtt://robodomo"),
+  console = require("console");
 
 function main() {
-    const cleaned = {}
-    client.subscribe(['undefined/#', 'smartthings/Watch Smart TV [Harmony Activity]/#'])
-    client.on('message', (topic, message) => {
-        console.log('message', topic)
-        if (!cleaned[topic]) {
-            client.publish(topic, '', { retain: true})
-            cleaned[topic] = true
-        }
-    })
+  const cleaned = {};
+  client.subscribe([
+    "undefined/#",
+    "smartthings/Watch Smart TV [Harmony Activity]/#"
+  ]);
+  client.on("message", (topic /*message*/) => {
+    console.log("message", topic);
+    if (!cleaned[topic]) {
+      client.publish(topic, "", { retain: true });
+      cleaned[topic] = true;
+    }
+  });
 }
 
-main()
+main();
